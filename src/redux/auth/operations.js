3afbb8010ba4a3a4c198825222f7d4fbe6c15084
Dima2430 <142937464+Dima2430@users.noreply.github.com>
2024-08-16 13:41:14 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+axios.defaults.baseURL = "https://nodejs-hw-mongodb-jzmm.onrender.com";
 export const setToken = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
